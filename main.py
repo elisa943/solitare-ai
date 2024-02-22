9dd96844bf364f049774c3dfdaf0e6923a57e82b
Elisa Chien <echien@bordeaux-inp.fr>
2024-02-22 21:16:59 +0100

    Ajout de plusieurs méthodes

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ class Suit(Enum):
 class Deck():
     def __init__(self):
         self.stockpile = []
+        self.cardsShown = []
 
         # Adds all possible cards
         for i in range(14):
@@ -69,6 +70,28 @@ class Deck():
             self.stockpile.append(card)
 
         return cardPicked
+    
+    def reinitialize_deck(self):
+        # If the deck is empty, re-initialize it
+        if len(self.stockpile) == 0:
+            self.stockpile = self.cardsShown.reverse()
+            self.cardsShown = []
+
+        # Else, does nothing
+
+    def shows_3_cards(self):
+        """
+        Displays up to 3 cards by adding them to self.cardsShown
+        """
+
+        # If impossible to display new cards, re-initialize the deck
+        if len(self.stockpile) == 0:
+            raise ImplementationError
+        
+        # Else, adds up to 3 cards the cardsShown list
+        for _ in range(min(3, len(self.stockpile))):
+            newCard = self.picks_card()
+            self.cardsShown.append(newCard)
 
 
 class Table():
@@ -143,13 +166,15 @@ class Table():
         for card in stack:
             self.cardsOnTable[index].append(card)
 
-    def makes_move(self, move):
+    def makes_move_in_table(self, move):
         """
-        Returns True if move is made. 
+        Returns True if the move is made. 
         'move' is a tuple containing (source, destination, card) with : 
             - source : int, index of the table 
             - destination : int, index of the table 
             - card : (Rank, Suit), card
+
+        The move is relative to the table and not the foundation piles
         """
         source = move[0]
         destination = move[1]
@@ -205,6 +230,22 @@ class FoundationPiles():
                 return False
 
         return True 
+    
+    def adds_to_piles(self, card):
+        """
+        Returns True of the card can be added on top of the pile foundation
+        """
+        rank = card[0]
+        suit = card[1]
+
+        # If card can be added on pile foundation, then it's added and returns True
+        if self.cardsOnPiles[suit] == rank - 1:
+            self.cardsOnPiles[suit] += 1
+            return True
+        else:
+            return False
+
+
 
 
 class Player():
@@ -212,6 +253,19 @@ class Player():
         self.deck = deck
         self.table = table
         self.foundationPiles = foundationPiles
+    
+    def possible_actions(self):
+        """
+        TODO : Returns list of possibles actions
+        """
+        possibleActions = []
+
+        # ! Check if cards shown on deck can be used 
+
+        # ! Checks if cards in the table can be moved 
+            # Checks if cards can be moved inside the table or onto the foundation pile
+        
+        return possible_actions
 
 
 class PlayerAI():
