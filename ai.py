87538d93a2f1da1463e168d7fb1a8737eb92ebf9
Elisa Chien <elisa942003@gmail.com>
2024-02-29 10:01:59 +0100

    Ajout du fichier ai.py

---
+++ ai.py
@@ -0,0 +1,18 @@
+class PlayerAI():
+    def __init__(self, deck, table, foundationPiles):
+        self.deck = deck
+        self.table = table
+        self.foundationPiles = foundationPiles
+
+    def possible_actions(self):
+        """
+        TODO : Returns list of possibles actions
+        """
+        possibleActions = []
+
+        # ! Check if cards shown on deck can be used 
+
+        # ! Checks if cards in the table can be moved 
+            # Checks if cards can be moved inside the table or onto the foundation pile
+        
+        return possible_actions
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import time 
 from pygame import mixer
 from generate import * 
+from ai import * 
 
 class Player():
     def __init__(self, deck, table, foundationPiles):
@@ -124,26 +125,6 @@ class Player():
         pygame.display.quit()
         pygame.quit()
         sys.exit()
-
-
-class PlayerAI():
-    def __init__(self, deck, table, foundationPiles):
-        self.deck = deck
-        self.table = table
-        self.foundationPiles = foundationPiles
-
-    def possible_actions(self):
-        """
-        TODO : Returns list of possibles actions
-        """
-        possibleActions = []
-
-        # ! Check if cards shown on deck can be used 
-
-        # ! Checks if cards in the table can be moved 
-            # Checks if cards can be moved inside the table or onto the foundation pile
-        
-        return possible_actions
 
 
 def main():
